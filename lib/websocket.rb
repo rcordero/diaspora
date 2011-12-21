b5b91f8e6bddc8cb895772113401802283bab6b0
Maxwell Salzberg <maxwell@joindiaspora.com>
2011-12-21 15:11:02 -0800

    wip removing websockets and replacing them with a dummy stub for now

---
+++ lib/websocket.rb
@@ -0,0 +1,15 @@
+class Websocket
+  def initialize(*args)
+  end
+
+  def self.to(*args)
+    w = Websocket.new(*args)
+    w
+  end
+
+  def send(object)
+  end
+
+  def retract(object)
+  end
+end
